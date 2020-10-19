463599b8d09f7935d5a7900668d0dc977086f81a
chandra <chandra@anterin.id>
2020-10-19 21:39:01 +0700

    fix page size

--- controllers/rulesController.js
+++ controllers/rulesController.js
@@ -96,9 +96,9 @@ exports.getData = async (req,res,next) => {
                 var data = await rulesRepo.getAllData(setting)
             }
             var info = {
-                page_size : parseInt(setting.pageSize),
+                page_size : setting.pageNo > 0 ? parseInt(setting.pageSize) : data.count,
                 page_no : parseInt(setting.pageNo),
-                total_page : Math.ceil(data.count / setting.pageSize)
+                total_page : setting.pageNo > 0? Math.ceil(data.count / setting.pageSize) :1
             }
             data = {...data,...info}
             res.json({
